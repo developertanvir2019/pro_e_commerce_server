b1c1409451d6e532d6d819ef03030a3c660c1163
developertanvir2019 <developertanvir2019@gmail.com>
2023-07-03 22:46:44 +0600

    middleware for authentication

--- src/server.js
+++ src/server.js
@@ -4,13 +4,20 @@ const app = express();
 app.use(morgan("dev"))
 
 const isLoggedIn = (req, res, next) => {
+    const login = true
+    if (login) {
+        next();
+    } else {
+        return res.status(401).json({ message: 'unauthorized user' })
+    }
 
 }
+app.use(isLoggedIn)
 app.get('/test', (req, res) => {
     res.status(200).send('server is running on pro e commerce')
 })
 
-app.get('/api/user', (req, res) => {
+app.get('/api/user', isLoggedIn, (req, res) => {
     res.status(200).send('server is running on pro e commerce')
 })
 
