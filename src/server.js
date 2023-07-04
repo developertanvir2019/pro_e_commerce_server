21d2556417055695b4990f1ec12768c7433ed796
developertanvir2019 <developertanvir2019@gmail.com>
2023-07-04 23:09:12 +0600

    add extra middleware

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
 const app = express();
-app.use(morgan("dev"))
-
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 const isLoggedIn = (req, res, next) => {
     const login = true
     if (login) {
