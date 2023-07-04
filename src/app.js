6365f27560ad8b342ccadf844cc617592053f58b
developertanvir2019 <developertanvir2019@gmail.com>
2023-07-04 23:38:57 +0600

    http-errors and rateLimit install and use

--- src/app.js
+++ src/app.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const createError = require('http-errors')
+const xssClean = require('xss-clean')
+const rateLimit = require('express-rate-limit')
+
+
 const app = express();
+
+const rateLimiter = rateLimit({
+    windowMs: 60 * 1000,
+    max: 3,
+    message: 'Too many request from this IP. Please try again later'
+})
+app.use(rateLimit())
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(xssClean());
+
 app.get('/test', (req, res) => {
     res.status(200).send('server is running on pro e commerce')
 })
@@ -15,13 +29,15 @@ app.get('/api/user', (req, res) => {
 
 // error handle client
 app.use((req, res, next) => {
-    res.status(404).json({ message: 'this route is not found' })
-    next()
+    // res.status(404).json({ message: 'this route is not found' })
+    next(createError(404, 'route is not found'))
 })
 // error handle server
 app.use((err, req, res, next) => {
-    console.log(err.stack);
-    res.status(500).json({ message: 'something break' })
-})
+    return res.status(err.status || 5000).json({
+        success: false,
+        message: err.message,
+    });
+});
 
 module.exports = app;
